279cf8b415b98737c56ea5b8cbfecd5b9ad05750
iqbal <iqbalrajput1998@gmail.com>
2021-12-27 22:01:02 +0530

    user logs

--- src/app/user/guest/services/user-logs.service.ts
+++ src/app/user/guest/services/user-logs.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { DeviceDetectorService } from 'ngx-device-detector';
+import { Observable } from 'rxjs';
+import { ResultModel } from '../../models/response/base.model';
+import { ApiService } from '../../services/api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserLogsService {
 
-  constructor() { }
+  ip_address:any;
+  url:any; 
+
+  constructor(
+    private deviceDetectorService: DeviceDetectorService,
+    private apiService: ApiService
+    ) {
+      this.loadScript('../assets/resources/js/clientInfo.js'); 
+     }
+    public getDeviceInfo(): any {
+      return this.deviceDetectorService.deviceType;
+    }
+    public getbrowserInfo(): any {
+      return this.deviceDetectorService.browser;
+    }
+    public getIpAddress(): any {
+      this.ip_address=localStorage.getItem('ipAddress');
+      return this.ip_address;
+    }
+    loadScript(url: string) {
+      const body = <HTMLDivElement>document.body;
+      const script = document.createElement('script');
+      script.innerHTML = '';
+      script.src = url;
+      script.async = false;
+      script.defer = true;
+      body.appendChild(script);
+    }
+    
+    user_logs(reqModel: any): Observable<ResultModel> {
+    const route = "/api/auth/user_logs";
+    return this.apiService.post<ResultModel>(route, reqModel);
+  }
 }
