b24cedfe2c06bf0f23d679f065481701fc0ac6a9
iqbal <iqbalrajput1998@gmail.com>
2022-01-16 20:16:14 +0530

    testing changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,7 @@ export const environment = {
   ftpURL:  'https://admin.housingstreet.com/storage/',
   siteURL: 'https://new.housingstreet.com',
   googleURL: 'http://localhost:8000/api/auth/redirect/',
-  Paytm_formURL: 'https://securegw-stage.paytm.in/order/process'
+  Paytm_formURL: 'https://securegw-stage.paytm.in/order/process',
+  you_tube_url:'https://www.youtube-nocookie.com/embed/',
+  google_map_url:'https://maps.google.co.in/maps?q='
 };
