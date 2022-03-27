b63939dc0ceb1eeff0582bbbb274e02b0af003ca
sumen <sumen111mallick@gmail.com>
2022-03-27 10:56:32 +0530

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,26 @@
+// export const environment = {
+//   production: true,
+//   host:'https://admin.housingstreet.com',
+//   apiUrl: 'https://admin.housingstreet.com',
+//   ftpURL:  'https://admin.housingstreet.com/storage/',
+//   siteURL: 'https://housingstreet.com',
+//   googleURL: 'https://admin.housingstreet.com/api/auth/redirect/',
+//   Paytm_formURL: 'https://securegw.paytm.in/order/process',
+//   you_tube_url:'https://www.youtube-nocookie.com/embed/',
+//   google_map_url:'https://maps.google.co.in/maps?q=',
+//   toll_free: '18005475450'
+// };
+
 export const environment = {
   production: true,
-  host:'https://admin.housingstreet.com',
-  apiUrl: 'https://admin.housingstreet.com',
-  ftpURL:  'https://admin.housingstreet.com/storage/',
-  siteURL: 'https://new.housingstreet.com',
-  googleURL: 'http://localhost:8000/api/auth/redirect/',
+  host:'https://admindevelopment.housingstreet.com',
+  apiUrl: 'https://admindevelopment.housingstreet.com',
+  ftpURL:  'https://admindevelopment.housingstreet.com/storage/',
+  siteURL: 'https://development.housingstreet.com',
+  googleURL: 'https://admindevelopment.housingstreet.com/api/auth/redirect/',
   Paytm_formURL: 'https://securegw-stage.paytm.in/order/process',
   you_tube_url:'https://www.youtube-nocookie.com/embed/',
   google_map_url:'https://maps.google.co.in/maps?q=',
-  toll_free: '18005475450'
+  toll_free: '18005475450',
+  App_version:'v2.0.1'
 };
