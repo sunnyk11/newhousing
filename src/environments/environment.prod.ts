d9d7289cde02707c12a23ae5a739abdf6238f20d
sumen <sumen111mallick@gmail.com>
2022-04-07 01:00:58 +0530

    prod cred

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,26 +1,27 @@
-// export const environment = {
-//   production: true,
-//   host:'https://admin.housingstreet.com',
-//   apiUrl: 'https://admin.housingstreet.com',
-//   ftpURL:  'https://admin.housingstreet.com/storage/',
-//   siteURL: 'https://housingstreet.com',
-//   googleURL: 'https://admin.housingstreet.com/api/auth/redirect/',
-//   Paytm_formURL: 'https://securegw.paytm.in/order/process',
-//   you_tube_url:'https://www.youtube-nocookie.com/embed/',
-//   google_map_url:'https://maps.google.co.in/maps?q=',
-//   toll_free: '18005475450'
-// };
-
 export const environment = {
   production: true,
-  host:'https://admindevelopment.housingstreet.com',
-  apiUrl: 'https://admindevelopment.housingstreet.com',
-  ftpURL:  'https://admindevelopment.housingstreet.com/storage/',
-  siteURL: 'https://staging.housingstreet.com',
-  googleURL: 'https://admindevelopment.housingstreet.com/api/auth/redirect/',
-  Paytm_formURL: 'https://securegw-stage.paytm.in/order/process',
+  host:'https://admin.housingstreet.com',
+  apiUrl: 'https://admin.housingstreet.com',
+  ftpURL:  'https://admin.housingstreet.com/storage/',
+  siteURL: 'https://housingstreet.com',
+  googleURL: 'https://admin.housingstreet.com/api/auth/redirect/',
+  Paytm_formURL: 'https://securegw.paytm.in/order/process',
   you_tube_url:'https://www.youtube-nocookie.com/embed/',
   google_map_url:'https://maps.google.co.in/maps?q=',
   toll_free: '18005475450',
   App_version:'v2.0.4'
 };
+
+// export const environment = {
+//   production: true,
+//   host:'https://admindevelopment.housingstreet.com',
+//   apiUrl: 'https://admindevelopment.housingstreet.com',
+//   ftpURL:  'https://admindevelopment.housingstreet.com/storage/',
+//   siteURL: 'https://staging.housingstreet.com',
+//   googleURL: 'https://admindevelopment.housingstreet.com/api/auth/redirect/',
+//   Paytm_formURL: 'https://securegw-stage.paytm.in/order/process',
+//   you_tube_url:'https://www.youtube-nocookie.com/embed/',
+//   google_map_url:'https://maps.google.co.in/maps?q=',
+//   toll_free: '18005475450',
+//   App_version:'v2.0.4'
+// };
