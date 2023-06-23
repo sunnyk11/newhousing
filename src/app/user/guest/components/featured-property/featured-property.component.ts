a77d6db51e1c12381f331c4bad98a982c9e6f171
iqbal <iqbalrajput1998@gmail.com>
2023-06-23 21:56:05 +0530

    testing

--- src/app/user/guest/components/featured-property/featured-property.component.ts
+++ src/app/user/guest/components/featured-property/featured-property.component.ts
@@ -54,7 +54,7 @@ export class FeaturedPropertyComponent implements OnInit {
     }
     return num;
   }
-  navigate(id:number,name:string,city:string,district:string,locality:string,sublocality:string,flat_type:string){
+  navigate(id:number,name:string,city:string,district:string,locality:string,sublocality:string,flat_type:string ){
     const url:any = this.router.createUrlTree(['/product-details'],{queryParams:{'id':id,'name':name,'city':city,'district':district,'locality':locality,'sublocality':sublocality,'flat-type':flat_type}})
     window.open(url.toString(), '_self')
   }
